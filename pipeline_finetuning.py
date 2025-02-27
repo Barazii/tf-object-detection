21c95ffe0d22ef9c0c9bba0749f1029c5e743d95
Barazii <mahmoud.baraziii@gmail.com>
2025-02-27 02:18:50 +0100

    Deleted all the code of modifying the model artifacts for fine tuning. It's not needed. Apparently sagemaker automatically deduces the number of classes and the classes itself from the input data and includes them in the output finetuned model that will be deployed and used for making inferences.

--- pipeline_finetuning.py
+++ pipeline_finetuning.py
@@ -11,12 +11,8 @@ from sagemaker.processing import ScriptProcessor
 from sagemaker import image_uris, model_uris, script_uris, hyperparameters
 import boto3
 import tempfile
-import tarfile
-import json
 import subprocess
 from pathlib import Path
-import pandas as pd
-import shutil
 
 
 def run_pipeline():
@@ -74,17 +70,18 @@ def run_pipeline():
                 output_name="validation",
             ),
             ProcessingOutput(
-                source=os.path.join(os.environ["PC_BASE_DIR"], "model_input_feed"),
+                source=os.path.join(os.environ["PC_BASE_DIR"], "finetuning_input"),
                 destination=os.path.join(
-                    os.environ["S3_PROJECT_URI"], "processing-step/model_input_feed"
+                    os.environ["S3_PROJECT_URI"], "finetuning/finetuning_input"
                 ),
-                output_name="model_input_feed",
+                output_name="finetuning_input",
             ),
         ],
         code="src/processing.py",
         cache_config=cache_config,
     )
 
+    # training and finetuning
     image_uri = image_uris.retrieve(
         region=os.environ["AWS_REGION"],
         framework=None,
@@ -103,65 +100,10 @@ def run_pipeline():
         model_version=os.environ["MODEL_VERSION"],
         model_scope="training",
     )
-    # modify the model artifacts to include the new classes
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        subprocess.run(
-            [
-                "aws",
-                "s3",
-                "cp",
-                model_uri,
-                f"{temp_dir}/",
-            ],
-            check=True,
-        )
-        zipped_model_fn = next(p for p in os.listdir(temp_dir) if p.endswith(".tar.gz"))
-        model_path = os.path.join(temp_dir, zipped_model_fn)
-        with tarfile.open(model_path, "r:gz") as tar:
-            tar.extractall(temp_dir)
-        # the labels json file
-        labels_json_file = temp_dir / "labels_info.json"
-        num_classes = 0
-        with open(labels_json_file, "r+") as f:
-            labels_dicttionary = json.load(f)
-            labels_dicttionary["labels"] = [
-                v[-1] for _, v in classes_mapping.items()
-            ] + labels_dicttionary["labels"]
-            f.seek(0)
-            json.dump(labels_dicttionary, f)
-            f.truncate()
-            num_classes = len(labels_dicttionary["labels"])
-        # the pipeline config file
-        config_file = temp_dir / "pipeline.config"
-        import re
-
-        with open(config_file, "r+") as f:
-            config_content = f.read()
-            config_content = re.sub(
-                r"num_classes:\s*\d+", f"num_classes: {num_classes}", config_content
-            )
-            f.seek(0)
-            f.write(config_content)
-            f.truncate()
-        modified_model_path = temp_dir / "model.tar.gz"
-        with tarfile.open(modified_model_path, "w:gz") as tar:
-            for item in os.listdir(temp_dir):
-                if item.endswith(".tar.gz"):
-                    continue
-                item_path = temp_dir / item
-                tar.add(item_path, arcname=os.path.relpath(item_path, temp_dir))
-        # upload the modified model
-        s3.upload_file(
-            modified_model_path,
-            os.environ["S3_BUCKET_NAME"],
-            "finetuning/model/model.tar.gz",
-        )
     hp = hyperparameters.retrieve_default(
         model_id=os.environ["MODEL_ID"], model_version=os.environ["MODEL_VERSION"]
     )
 
-    from sagemaker.estimator import Estimator
     from sagemaker.utils import name_from_base
 
     training_job_name = name_from_base("tf-birds-detection-transfer-learning")
@@ -175,7 +117,7 @@ def run_pipeline():
     estimator = Estimator(
         image_uri=image_uri,
         source_dir=source_uri,
-        model_uri=os.environ["S3_FINETUNING_MODEL_URI"],
+        model_uri=model_uri,
         entry_point="transfer_learning.py",
         role=os.environ["SM_EXEC_ROLE"],
         max_run=360000,
@@ -195,7 +137,7 @@ def run_pipeline():
             inputs={
                 "training": TrainingInput(
                     s3_data=processing_step.properties.ProcessingOutputConfig.Outputs[
-                        "model_input_feed"
+                        "finetuning_input"
                     ].S3Output.S3Uri,
                     content_type="application/x-image",
                     s3_data_type="S3Prefix",
@@ -206,7 +148,7 @@ def run_pipeline():
         depends_on=[processing_step],
     )
 
-    # create a sagemaker model instance out of the finetuned model
+    # create a sagemaker model instance out of the finetuned model to be deployed for inference
     image_uri = image_uris.retrieve(
         region=os.environ["AWS_REGION"],
         framework=None,
@@ -220,6 +162,7 @@ def run_pipeline():
         model_version=os.environ["MODEL_VERSION"],
         script_scope="inference",
     )
+    # download sourcedir, upalod to s3 (otherwise error for some reason)
     tmp_dir = Path(tempfile.mkdtemp())
     sourcedir = tmp_dir / "sourcedir"
     sourcedir.mkdir(exist_ok=True)
@@ -233,6 +176,7 @@ def run_pipeline():
         ],
         check=True,
     )
+    s3 = boto3.client("s3")
     s3.upload_file(
         f"{sourcedir}/sourcedir.tar.gz",
         os.environ["S3_BUCKET_NAME"],
