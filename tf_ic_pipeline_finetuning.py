fc2efad69920431d997d8095bc85674f046ee539
Barazii <mahmoud.baraziii@gmail.com>
2025-03-29 18:29:15 +0100

    Added the pipeline (training/learning transfer) for finetuning the tf ic model.

--- tf_ic_pipeline_finetuning.py
+++ tf_ic_pipeline_finetuning.py
@@ -6,10 +6,20 @@ import os
 from sagemaker.workflow.steps import CacheConfig
 from sagemaker.processing import ScriptProcessor
 from sagemaker.workflow.pipeline import Pipeline
+from sagemaker.model import Model
+from sagemaker.workflow.model_step import ModelStep
+from sagemaker import image_uris, model_uris, script_uris, hyperparameters
+from pathlib import Path
+import tempfile
+import subprocess
+import boto3
+from sagemaker.estimator import Estimator
+from sagemaker.workflow.steps import TrainingStep
+from sagemaker.inputs import TrainingInput
 
 
 def run_pipeline():
-    load_dotenv()
+    load_dotenv(override=True)
 
     cache_config = CacheConfig(enable_caching=False, expire_after="10d")
 
@@ -74,12 +84,125 @@ def run_pipeline():
         cache_config=cache_config,
     )
 
+    # training and finetuning
+    image_uri = image_uris.retrieve(
+        region=os.environ["AWS_REGION"],
+        framework=None,
+        model_id=os.environ["MODEL_ID"],
+        model_version=os.environ["MODEL_VERSION"],
+        image_scope="training",
+        instance_type=os.environ["TRAINING_INSTANCE_TYPE"],
+    )
+    source_uri = script_uris.retrieve(
+        model_id=os.environ["MODEL_ID"],
+        model_version=os.environ["MODEL_VERSION"],
+        script_scope="training",
+    )
+    model_uri = model_uris.retrieve(
+        model_id=os.environ["MODEL_ID"],
+        model_version=os.environ["MODEL_VERSION"],
+        model_scope="training",
+    )
+    hp = hyperparameters.retrieve_default(
+        model_id=os.environ["MODEL_ID"], model_version=os.environ["MODEL_VERSION"]
+    )
+    hp["batch_size"] = 8
+    hp["epochs"] = 7
+
+    training_metric_definitions = [
+        {"Name": "train:accuracy", "Regex": "accuracy: ([0-9\\.]+)"},
+        {"Name": "train:loss", "Regex": "loss: ([0-9\\.]+)"},
+        {"Name": "validation:accuracy", "Regex": "val_accuracy: ([0-9\\.]+)"},
+        {"Name": "validation:loss", "Regex": "val_loss: ([0-9\\.]+)"},
+    ]
+
+    estimator = Estimator(
+        image_uri=image_uri,
+        source_dir=source_uri,
+        model_uri=model_uri,
+        entry_point="transfer_learning.py",
+        role=os.environ["SM_EXEC_ROLE"],
+        max_run=360000,
+        instance_count=int(os.environ["TRAINING_INSTANCE_COUNT"]),
+        instance_type=os.environ["TRAINING_INSTANCE_TYPE"],
+        input_mode="File",
+        base_job_name="no-hpo",
+        sagemaker_session=sagemaker_session,
+        hyperparameters=hp,
+        metric_definitions=training_metric_definitions,
+    )
+
+    training_step = TrainingStep(
+        name="transfer-learning",
+        step_args=estimator.fit(
+            inputs={
+                "training": TrainingInput(
+                    s3_data=os.path.join(os.environ["S3_PROJECT_URI"], "finetuning/input_data"),
+                    content_type="application/x-image",
+                    s3_data_type="S3Prefix",
+                ),
+            },
+        ),
+        cache_config=cache_config,
+    )
+
+    # create a sagemaker model instance out of the finetuned model to be deployed for inference
+    image_uri = image_uris.retrieve(
+        region=os.environ["AWS_REGION"],
+        framework=None,
+        image_scope="inference",
+        model_id=os.environ["MODEL_ID"],
+        model_version=os.environ["MODEL_VERSION"],
+        instance_type=os.environ["INFERENCE_INSTANCE_TYPE"],
+    )
+    sourcedir_uri = script_uris.retrieve(
+        model_id=os.environ["MODEL_ID"],
+        model_version=os.environ["MODEL_VERSION"],
+        script_scope="inference",
+    )
+    # download sourcedir, upalod to s3 (otherwise error for some reason)
+    tmp_dir = Path(tempfile.mkdtemp())
+    sourcedir = tmp_dir / "sourcedir"
+    sourcedir.mkdir(exist_ok=True)
+    subprocess.run(
+        [
+            "aws",
+            "s3",
+            "cp",
+            sourcedir_uri,
+            f"{sourcedir}/",
+        ],
+        check=True,
+    )
+    s3 = boto3.client("s3")
+    s3.upload_file(
+        f"{sourcedir}/sourcedir.tar.gz",
+        os.environ["S3_BUCKET_NAME"],
+        "finetuning/sourcedir/sourcedir.tar.gz",
+    )
+
+    model = Model(
+        image_uri=image_uri,
+        source_dir=os.path.join(
+            os.environ["S3_PROJECT_URI"], "finetuning/sourcedir/sourcedir.tar.gz"
+        ),
+        model_data=training_step.properties.ModelArtifacts.S3ModelArtifacts,
+        entry_point="inference.py",
+        role=os.environ["SM_EXEC_ROLE"],
+        name="tf-ic-birds-finetuned",
+        sagemaker_session=sagemaker_session,
+    )
+
+    model_step = ModelStep(
+        name="create-model",
+        step_args=model.create(instance_type=os.environ["PROCESSING_INSTANCE_TYPE"]),
+        depends_on=[training_step],
+    )
+
     # build the pipeline
     pipeline = Pipeline(
         name="tf-birds-detection-pipeline",
-        steps=[
-            processing_step,
-        ],
+        steps=[training_step, model_step],
         sagemaker_session=sagemaker_session,
     )
 
